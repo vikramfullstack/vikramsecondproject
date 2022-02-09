fae44dc927e3a50310b010747b02837042fa805b
vikramfullstack <vikramfullstack247@gmail.com>
2022-02-09 12:45:05 +0530

    Core .csproj added to root in second project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace secondproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
